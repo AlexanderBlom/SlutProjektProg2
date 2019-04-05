05a1ed5be711af9c6197849b01d8e732ea5effc4
blal16 <te16.alexanderblom@umea.nti.se>
2019-04-05 12:25:58 +0200

    Test program för klienten

--- src/Client.java
+++ src/Client.java
@@ -1,7 +1,36 @@
 import javax.swing.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
 
 public class Client {
     public static void main(String[] args) {
-        String ip = JOptionPane.showInputDialog(null, "IP?")
+        String ip = (String) JOptionPane.showInputDialog(null, "IP?", "Connect to...", JOptionPane.QUESTION_MESSAGE);
+        int port = Integer.parseInt(JOptionPane.showInputDialog(null, "port", "Connect to..", JOptionPane.QUESTION_MESSAGE));
+        Socket socket = null;
+
+        try {
+            socket = new Socket(ip, port);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            PrintWriter out = new PrintWriter(socket.getOutputStream());
+            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+            out.println(JOptionPane.showInputDialog(null, "Name?" ,"Identify yourself"));
+
+            String msg = in.readLine();
+            JOptionPane.showMessageDialog(null, msg, "Server said", JOptionPane.INFORMATION_MESSAGE);
+
+            in.close();
+            out.close();
+            socket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
